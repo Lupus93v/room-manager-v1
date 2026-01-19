507ed9a8ded4c8b9b6b9f04d4698179e3281e238
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2026-01-19 21:19:14 +0100

    test
    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React HELLO!!!
+          Ovo će biti HomePage.
         </a>
       </header>
     </div>
