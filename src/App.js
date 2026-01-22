70c8483b27084cdfdb75b809920929ab534ccbff
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2026-01-22 10:24:29 +0100

    App.js code import
    new code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import PageLayout from "../src/components/PageLayout";
+import HomePage from "./Pages/HomePage";
 
 export default function App() {
   return (
-        <h1>
-          Ovo će biti HomePage.
-        </h1>
-  );
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageLayout/>}>
+          <Route index element={<HomePage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )}
